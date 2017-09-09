f6d56180b608bd0da18e553dce70709cf9a82b3a
bumpmakers <social@bumpmakers.com.br>
2017-09-09 16:30:16 +0000

    Deepin

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'bump-makers',
+		wp_ws_url : 'http://bumpmakers.com.br/wp-appkit-api/bump-makers',
+		wp_url : 'http://bumpmakers.com.br',
+		theme : 'q-android',
+		version : '1',
+		app_title : 'Bump Maker\'s',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
